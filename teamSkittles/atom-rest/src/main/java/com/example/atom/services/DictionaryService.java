c1f21845b24f4104f48eff28f9f70843d83f1474
AtomSkittlesTeam <sergej.davidyuk.2@gmail.com>
2023-03-17 11:21:30 +0300

    contractor init - заготовка

---
+++ teamSkittles/atom-rest/src/main/java/com/example/atom/services/DictionaryService.java
@@ -0,0 +1,126 @@
+package com.example.atom.services;
+
+import com.example.atom.dto.ContractorDto;
+import com.example.atom.dto.DemoDto;
+import com.example.atom.entities.Contractor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DictionaryService {
+
+    private String serverUrl = "server";
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public List<ContractorDto> getContractors() {
+        String url = UriComponentsBuilder.fromHttpUrl(serverUrl + "/dict/contractors")
+                .build(false)
+                .toUriString();
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(null, headers);
+
+            ResponseEntity<List<DemoDto>> responseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<ContractorDto>>() {
+                    });
+
+            List<ContractorDto> result = responseEntity.getBody();
+            if (result == null)
+                System.out.println("Null");
+            return result;
+        } catch (Exception e) {
+            System.out.println("impossible");
+        }
+        return null;
+    }
+
+    public List<DemoDto> sendIdToServer(Long id) {
+        String url = UriComponentsBuilder.fromHttpUrl(serverUrl + "/integration/send-id/" + id.toString())
+                .build(false)
+                .toUriString();
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(null, headers);
+            //HttpEntity<?> entity = new HttpEntity<>(id, headers); //как вариант, id здесь может быть
+
+            ResponseEntity<List<DemoDto>> responseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<DemoDto>>() {
+                    });
+
+            List<DemoDto> result = responseEntity.getBody();
+            if (result == null)
+                System.out.println("Null");
+            return result;
+        } catch (Exception e) {
+            System.out.println("impossible");
+        }
+        return null;
+    }
+
+    public List<DemoDto> sendDemoToServer(DemoDto demoDto) {
+        String url = UriComponentsBuilder.fromHttpUrl(serverUrl + "/integration/send-dto")
+                .build(false)
+                .toUriString();
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(demoDto, headers);
+
+            ResponseEntity<List<DemoDto>> responseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    entity,
+                    new ParameterizedTypeReference<List<DemoDto>>() {
+                    });
+
+            List<DemoDto> result = responseEntity.getBody();
+            if (result == null)
+                System.out.println("Null");
+            return result;
+        } catch (Exception e) {
+            System.out.println("impossible");
+        }
+        return new ArrayList<>();
+    }
+
+    public List<DemoDto> sendDemoListToServer(List<DemoDto> demoDtoList) {
+        String url = UriComponentsBuilder.fromHttpUrl(serverUrl + "/integration/send-dto")
+                .build(false)
+                .toUriString();
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(demoDtoList, headers);
+
+            ResponseEntity<List<DemoDto>> responseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    entity,
+                    new ParameterizedTypeReference<List<DemoDto>>() {
+                    });
+
+            List<DemoDto> result = responseEntity.getBody();
+            if (result == null)
+                System.out.println("Null");
+            return result;
+        } catch (Exception e) {
+            System.out.println("impossible");
+        }
+        return new ArrayList<>();
+    }
+
+}
